930c3405dbde6a283d0ea52c20d1bc88eb36426c
Alex-Nicalace <anicalace@gmail.com>
2025-08-15 17:08:26 +0300

    feat: import *.svg as React component

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   sassOptions: {
     additionalData: `@use 'src/app/styles/module-tools.scss' as *;`,
   },
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.ts',
+      },
+    },
+  },
 };
 
 export default nextConfig;
