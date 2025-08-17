9a074b10cb01a0df06444cab8d9673458694f30c
Alex-Nicalace <anicalace@gmail.com>
2025-08-17 18:51:07 +0300

    feat(shared/Icon): add prop color

--- src/shared/ui/Icon/Icon.tsx
+++ src/shared/ui/Icon/Icon.tsx
@@ -1,11 +1,25 @@
+import clsx from 'clsx';
+
 import { iconMap, IconName } from './iconMap';
+
+import styles from './Icon.module.scss';
+
+type IconColor = 'regular' | 'active';
 
 type IconProps = React.SVGProps<SVGSVGElement> & {
   name: IconName;
+  color?: IconColor;
 };
 
-export function Icon({ name, ...props }: IconProps) {
+export function Icon({
+  name,
+  className,
+  color = 'regular',
+  ...props
+}: IconProps) {
   const SvgIcon = iconMap[name];
 
-  return <SvgIcon {...props} />;
+  return (
+    <SvgIcon className={clsx(className, styles[`${color}Color`])} {...props} />
+  );
 }
