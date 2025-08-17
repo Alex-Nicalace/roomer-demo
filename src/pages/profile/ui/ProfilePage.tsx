608ecc6a5cefaaef07795c0f823ffd59e4f6e159
Alex-Nicalace <anicalace@gmail.com>
2025-08-17 18:50:10 +0300

    feat: add route /profile

---
+++ src/pages/profile/ui/ProfilePage.tsx
@@ -0,0 +1,13 @@
+import { Header } from 'widgets/Header';
+
+// type pageProps = { }
+export default function ProfilePage(/*{ }: pageProps*/) {
+  return (
+    <>
+      <Header>
+        <h1>Профиль</h1>
+      </Header>
+      <main>ProfilePage</main>
+    </>
+  );
+}
