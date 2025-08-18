cf0043f04604984862ddb93875924244ed6ab4ae
Alex-Nicalace <anicalace@gmail.com>
2025-08-18 15:07:22 +0300

    fix(widget/footer): style padding

--- src/widgets/Footer/ui/Footer.tsx
+++ src/widgets/Footer/ui/Footer.tsx
@@ -2,15 +2,13 @@ import clsx from 'clsx';
 
 import { FooterNav } from './FooterNav';
 
-import styles from './Footer.module.scss';
-
 type FooterProps = {
   className?: string;
 };
 
 export default function Footer({ className }: FooterProps) {
   return (
-    <footer className={clsx(styles.footer, className)}>
+    <footer className={clsx(className)}>
       <FooterNav />
     </footer>
   );
