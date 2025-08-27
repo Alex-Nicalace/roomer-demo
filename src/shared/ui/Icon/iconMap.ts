1de4d9497eff815bd5a616288fece9d874630460
Alex-Nicalace <anicalace@gmail.com>
2025-08-27 16:59:23 +0300

    refactor(icons): rename icon exit to share

--- src/shared/ui/Icon/iconMap.ts
+++ src/shared/ui/Icon/iconMap.ts
@@ -4,7 +4,7 @@ import FavouritesIcon from 'shared/assets/icons/favourites.svg';
 import GlobeIcon from 'shared/assets/icons/globe.svg';
 import MessageIcon from 'shared/assets/icons/message.svg';
 import CatIcon from 'shared/assets/icons/cat.svg';
-import ExitIcon from 'shared/assets/icons/exit.svg';
+import ShareIcon from 'shared/assets/icons/share.svg';
 import EyeIcon from 'shared/assets/icons/eye.svg';
 import GearIcon from 'shared/assets/icons/gear.svg';
 import LeftArrowIcon from 'shared/assets/icons/left-arrow.svg';
@@ -16,7 +16,7 @@ export const iconMap = {
   globe: GlobeIcon,
   message: MessageIcon,
   cat: CatIcon,
-  exit: ExitIcon,
+  share: ShareIcon,
   eye: EyeIcon,
   gear: GearIcon,
   leftArrow: LeftArrowIcon,
