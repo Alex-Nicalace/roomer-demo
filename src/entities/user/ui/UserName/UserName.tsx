5e933867dbe7e7a081136994bf6c09a5b7e7405d
Alex-Nicalace <anicalace@gmail.com>
2025-08-28 15:37:16 +0300

    feat(user-name): add UserName component with styles

---
+++ src/entities/user/ui/UserName/UserName.tsx
@@ -0,0 +1,18 @@
+import clsx from 'clsx';
+
+import styles from './UserName.module.scss';
+
+type UserNameProps = {
+  className?: string;
+};
+export default function UserName({ className }: UserNameProps) {
+  //todo: replace with real data
+  const role = 'румер';
+  const fullName = 'Ваня Петькин';
+  return (
+    <div className={clsx(styles.userName, className)}>
+      <span className={styles.role}>{`${role}: `}</span>
+      <span className={styles.fullName}>{fullName}</span>
+    </div>
+  );
+}
