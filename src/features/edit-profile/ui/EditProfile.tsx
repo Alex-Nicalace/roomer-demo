d7404af44afcbac936acb86f28b7a8d2970ebe4a
Alex-Nicalace <anicalace@gmail.com>
2025-08-29 17:55:17 +0300

    feat(edit-profile): add EditProfile

---
+++ src/features/edit-profile/ui/EditProfile.tsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx';
+import { Button, ButtonTheme } from 'shared/ui/button';
+import styles from './EditProfile.module.scss';
+
+type EditProfileProps = {
+  className?: string;
+};
+export default function EditProfile({ className }: EditProfileProps) {
+  //* Должна быть логика действия, раз это фича.
+  //* Вожможно модалку открыть с формой редактирования
+
+  return (
+    <Button
+      className={clsx(styles.EditProfile, className)}
+      withIcon="gear"
+      theme={ButtonTheme.ICON_OUTLINE}
+    >
+      редакт
+    </Button>
+  );
+}
